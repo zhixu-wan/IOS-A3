e565cdacad1d86e15b3354747ca2d6f2186aacf0
Zhixu Wan <105278362+zhixu-wan@users.noreply.github.com>
2022-05-12 16:57:46 +0800

    add logo

---
+++ IOS_A3/RestaurantList.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantList.swift
+//  IOS_A3
+//
+//  Created by Zhixu Wan on 2022/5/12.
+//
+
+import Foundation
+
+import UIKit
+
+class RestaurantList: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+}
--- IOS_A3/ViewController.swift
+++ IOS_A3/ViewController.swift
@@ -12,8 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        logoImage()
     }
 
-
+    func logoImage() {
+        let logo = UIImageView(image: UIImage(named: "logo"))
+        logo.layer.frame = CGRect(x: view.layer.frame.midX-80, y: view.layer.frame.midY-300, width: 160, height: 160)
+        logo.layer.cornerRadius = 80
+        logo.clipsToBounds = true
+        
+        view.addSubview(logo)
+    }
 }
 
