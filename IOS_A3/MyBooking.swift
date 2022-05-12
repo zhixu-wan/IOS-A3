7fd922d2c40142f9ed9244d5de961e1b0775fe18
Zhixu Wan <105278362+zhixu-wan@users.noreply.github.com>
2022-05-12 22:09:09 +0800

    change

--- IOS_A3/MyBooking.swift
+++ IOS_A3/MyBooking.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class MyBooking: UIViewController {
 
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,12 +23,11 @@ class MyBooking: UIViewController {
 
 extension MyBooking: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        return cell
+        let cell = 
     }
     
     
