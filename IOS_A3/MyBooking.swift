25e4b9b40ab173323158a1ac10f261cb0751262e
Zhixu Wan <105278362+zhixu-wan@users.noreply.github.com>
2022-05-17 23:22:44 +0800

    update myBooking

--- IOS_A3/MyBooking.swift
+++ IOS_A3/MyBooking.swift
@@ -17,7 +17,7 @@ class MyBooking: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        appDelegate.updateCustomer()
     }
     
 }
