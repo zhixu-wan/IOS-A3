f5910a3c21e46797793df7b989920011391604a4
Zhixu Wan <105278362+zhixu-wan@users.noreply.github.com>
2022-05-17 22:38:53 +0800

    Revert "datecontentpicker"
    This reverts commit cbba9e0b7979937298766e630eeab095b607502e.

--- IOS_A3/DateContent.swift
+++ IOS_A3/DateContent.swift
@@ -1,59 +1,13 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by ZhengYZe on 2022/5/17.
-//
-
 import SwiftUI
 
-struct Datecontent: View {
-    @State var dateSelected = Date();
-    @State var time = Date();
-    
-    var dateFromatter:DateFormatter {
-        let df = DateFormatter()
-        df.dateStyle = .medium
-        return df
-    }
-    var timeFromatter:DateFormatter {
-        let df = DateFormatter()
-        df.dateStyle = .none
-        df.timeStyle = .medium
-        return df
-    }
-    let minDate = Calendar.current.date(byAdding: .day, value: 0 ,to: Date())!
-    let maxDate = Calendar.current.date(byAdding: .day, value: 20 ,to: Date())!
-    
+struct DateContent: View{
     var body: some View{
-        VStack{
-            Text("Today is: ")
-                .font(.title)
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.primary)
-            DatePicker("", selection: $dateSelected, in:minDate...maxDate, displayedComponents: .date).labelsHidden()
-                
-
-            
-            Text("Time selceted: \(time, formatter: timeFromatter)")
-            Form{
-                Section(header: Text("Date Picker")){
-                    Text("Date picker in form")
-                    DatePicker("Pick a date:", selection: $dateSelected, displayedComponents: .date)
-                    DatePicker("Pick time:", selection: $time, displayedComponents: .hourAndMinute)
+        NavigationView{
+            VStack{
+                DatePicker{
                     
                 }
             }
-            Spacer()
         }
     }
 }
-struct DateContent_Previews: PreviewProvider{
-    static var previews: some View{
-        Datecontent()
-            .preferredColorScheme(.dark)
-    }
-}
-
