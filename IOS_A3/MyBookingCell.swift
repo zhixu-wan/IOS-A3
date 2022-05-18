3dbe87564786b5616b9b83e2f4c81509eff022a7
Zhixu Wan <105278362+zhixu-wan@users.noreply.github.com>
2022-05-18 23:39:47 +0800

    revert to booking data

---
+++ IOS_A3/MyBookingCell.swift
@@ -0,0 +1,38 @@
+//
+//  MyBookingCell.swift
+//  IOS_A3
+//
+//  Created by Zhixu Wan on 2022/5/12.
+//
+
+import UIKit
+
+protocol myBookingCellDelegate {
+    func cancelReser(id: Int, status: String)
+}
+
+class MyBookingCell: UITableViewCell {
+    var delegate: myBookingCellDelegate?
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var restaurLabel: UILabel!
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBAction func cancel(_ sender: Any) {
+        delegate?.cancelReser(id: Int(idLabel.text!)!, status: statusLabel.text!)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
